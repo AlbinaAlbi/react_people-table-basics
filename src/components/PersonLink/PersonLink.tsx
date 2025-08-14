536308b4786fe320abcc8c1d6ab2313084a2e540
Albina <yakunina.albina@gmail.com>
2025-08-14 11:49:32 +0200

    impl table basics

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import { Person } from '../../types';
+
+interface PersonLinkProps {
+  name: string | null;
+  people: Person[];
+}
+
+export const PersonLink: React.FC<PersonLinkProps> = ({ name, people }) => {
+  if (!name) {
+    return <>-</>;
+  }
+
+  const person = people.find(p => p.name === name);
+
+  if (!person) {
+    return <>{name}</>;
+  }
+
+  return (
+    <NavLink
+      to={`/people/${person.slug}`}
+      className={person.sex === 'f' ? 'has-text-danger' : ''}
+    >
+      {person.name}
+    </NavLink>
+  );
+};
